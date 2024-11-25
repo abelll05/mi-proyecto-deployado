e5882694709b030d9dd9f8b48472bbdb805c093e
abelll05 <abelmerc45@gmail.com>
2024-11-25 03:43:21 -0300

    Primer commit de mi proyecto full-stack

--- src/api/myenv/Scripts/activate.bat
+++ src/api/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\regen\Desktop\mi proyecto\my-proyect\src\api\myenv
+set VIRTUAL_ENV=C:\Users\abelm\tutorial-100-pasos\src\api\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
